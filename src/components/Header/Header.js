e9664a6f3d4a77a696c50ecbfec82a837f49f742
Vincent Manzanera <vincent.mzapro@outlook.fr>
2023-11-04 19:10:16 +0100

    Font : Set font packages into the project at src/resources/font. Loaded packages in src/style/_font.scss

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 const Header = ()=>{
     return (
         <div id="header_id">
-            <p>SALUT LES MUSCLES !!</p>
+            <p>Teddy Bear</p>
         </div>
     )
 }
